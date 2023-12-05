ac2ec868bbeffacdaecc31b0e38df5ab2c00f123
btarg <icrazyblaze1@gmail.com>
2023-12-05 17:42:13 +0000

    Separate files

--- backend/frontend/src/App.vue
+++ backend/frontend/src/App.vue
@@ -1,46 +1,20 @@
 <!-- App.vue -->
 <template>
   <div id="app">
-    <h1>My Custom Object Form</h1>
-    <form @submit.prevent="submitForm">
-      <label for="testString">Test String:</label>
-      <input v-model="myCustomObject.testString" type="text" id="testString" />
-
-      <label for="testInt">Test Integer:</label>
-      <input v-model.number="myCustomObject.testInt" type="number" id="testInt" />
-
-      <button type="submit">Submit</button>
-    </form>
+    <MyCustomObjectForm />
   </div>
 </template>
 
 <script>
-import axios from "axios";
+import MyCustomObjectForm from './components/MyCustomObjectForm.vue';
 
 export default {
-  data() {
-    return {
-      myCustomObject: {
-        testString: '',
-        testInt: null,
-      },
-    };
-  },
-  methods: {
-    submitForm() {
-      // Send the MyCustomObject to the backend
-      axios.post('/api/receiveObject', this.myCustomObject)
-          .then(response => {
-            console.log('Object submitted successfully:', response.data);
-          })
-          .catch(error => {
-            console.error('Error submitting object:', error);
-          });
-    },
+  components: {
+    MyCustomObjectForm,
   },
 };
 </script>
 
-<style scoped>
-/* Add your custom styles here */
+<style>
+/* Add global styles here if needed */
 </style>
